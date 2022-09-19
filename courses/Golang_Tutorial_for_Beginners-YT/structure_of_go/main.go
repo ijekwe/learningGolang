b3ee32f741d2996f42655a27455531af33b27b4e
iheanyiekwe <contactiheanyi@gmail.com>
2022-09-19 14:48:56 -0500

    arrays

--- courses/Golang_Tutorial_for_Beginners-YT/structure_of_go/main.go
+++ courses/Golang_Tutorial_for_Beginners-YT/structure_of_go/main.go
@@ -7,11 +7,11 @@ func main() {
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
-
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
+
+	var bookings [50]string
 
 	var firstName string
 	var lastName string
@@ -31,6 +31,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 
 	userTickets = 2
 	fmt.Printf("Thank you %v %v for booking %v tickets. You  will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
