895faeae5b2d89c0c299140ba9b8ee972f98f6f6
iheanyiekwe <contactiheanyi@gmail.com>
2022-09-19 14:27:52 -0500

    book ticket (logic)

--- courses/Golang_Tutorial_for_Beginners-YT/structure_of_go/main.go
+++ courses/Golang_Tutorial_for_Beginners-YT/structure_of_go/main.go
@@ -5,18 +5,18 @@ import "fmt"
 func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	var remaingingTickets int = 50
+	var remainingTickets uint = 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remaingingTickets, conferenceName)
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
-	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remaingingTickets)
+	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
 
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for thier name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -30,7 +30,10 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	userTickets = 2
 	fmt.Printf("Thank you %v %v for booking %v tickets. You  will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
 }
