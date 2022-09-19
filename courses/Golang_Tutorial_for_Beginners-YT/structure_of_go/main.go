45fc7bb680ef1315a3cf60e7332f0f3570a9ece4
iheanyiekwe <contactiheanyi@gmail.com>
2022-09-19 15:02:23 -0500

    slices

--- courses/Golang_Tutorial_for_Beginners-YT/structure_of_go/main.go
+++ courses/Golang_Tutorial_for_Beginners-YT/structure_of_go/main.go
@@ -6,12 +6,11 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
-
-	var bookings [50]string
 
 	var firstName string
 	var lastName string
@@ -31,12 +30,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The whole slice: %v\n", bookings)
 	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
 
 	userTickets = 2
 	fmt.Printf("Thank you %v %v for booking %v tickets. You  will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
